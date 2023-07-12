d38b25f326225b98ed505884e774a0bc6d24e277
Eric Robsky Huntley <ehuntley@mit.edu>
2023-07-12 00:30:50 -0400

    register models with admin

--- map/admin.py
+++ map/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import State, County, Tract
 
 # Register your models here.
+admin.site.register(State)
+admin.site.register(County)
+admin.site.register(Tract)
