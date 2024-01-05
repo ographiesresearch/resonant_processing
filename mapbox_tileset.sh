a175e62e345d9a768fe3c5115a61996157dd8bf0
Eric Robsky Huntley <ericrobskyhuntley@gmail.com>
2024-01-05 13:49:32 -0500

    tweak recipe, tileset automation

--- mapbox_tileset.sh
+++ mapbox_tileset.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Automate the source creation and tileset publication process.
 
-tilesets upload-source $1 resonant-energy-geographies data/resonant_output.geojson;
-tilesets upload-source $1 resonant-energy-additional data/additional_criteria.geojson;
-tilesets create $1.resonant-energy --recipe results-recipe.json --name "Resonant Energy Geographies";
-tilesets publish $1.resonant-energy;
+TILESET=$1".resonant-energy"
+
+tilesets upload-source $1 resonant-energy-geographies data/resonant_results.geojson
+tilesets upload-source $1 resonant-energy-additional data/additional_criteria.geojson
+tilesets create $TILESET --recipe recipe.json --name 'Resonant Energy Geographies'
+tilesets publish $TILESET
