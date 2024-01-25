9e49ff54b06d73386a4db078d45534290559c30b
Eric Robsky Huntley <ericrobskyhuntley@gmail.com>
2024-01-25 10:41:39 -0500

    create attribution, make automation more robust

--- mapbox_tileset.sh
+++ mapbox_tileset.sh
@@ -2,8 +2,19 @@
 # Automate the source creation and tileset publication process.
 
 TILESET=$1".resonant"
+ATTR="`cat attr.json`"
 
-tilesets upload-source $1 resonant-energy-geographies data/resonant_results.geojson
-tilesets upload-source $1 resonant-energy-additional data/additional_criteria.geojson
-tilesets create $TILESET --recipe recipe.json --name 'Resonant Energy Geographies'
-tilesets publish $TILESET
+{
+  echo "Uploading results..."
+  tilesets upload-source $1 --replace resonant-geographies "data/resonant_results.geojson"
+  echo "Uploading additional selection criteria..."
+  tilesets upload-source $1 --replace resonant-additional "data/additional_criteria.geojson"
+} && { 
+  # If source upload is successful, create tileset.
+  echo "Creating tileset using recipe..."
+  tilesets create $TILESET --recipe recipe.json --name 'Resonant Energy Geographies' --attribution $ATTR
+} && {
+  echo "Starting publishing process..."
+  # If tileset creation is successful, publish tileset.
+  tilesets publish $TILESET
+}
