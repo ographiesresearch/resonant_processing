3450a47df382a3532de22241692bebd4f2526900
Eric Robsky Huntley <ericrobskyhuntley@gmail.com>
2024-01-25 11:31:40 -0500

    further tweaks to upload process (limited to 3 attributions)

--- upload_publish_mbt.sh
+++ upload_publish_mbt.sh
@@ -6,13 +6,18 @@ ATTR="`cat attr.json`"
 
 {
   echo "Uploading results..."
-  tilesets upload-source $1 --replace resonant-geographies "data/resonant_results.geojson"
+  tilesets upload-source $1 --replace --no-validation resonant-geographies "data/resonant_results.geojson"
   echo "Uploading additional selection criteria..."
-  tilesets upload-source $1 --replace resonant-additional "data/additional_criteria.geojson"
+  tilesets upload-source $1 --replace --no-validation resonant-additional "data/additional_criteria.geojson"
 } && { 
   # If source upload is successful, create tileset.
   echo "Creating tileset using recipe..."
-  tilesets create $TILESET --recipe recipe.json --name 'Resonant Energy Geographies' --attribution $ATTR
+  tilesets create $TILESET --recipe recipe.json --attribution '[
+    {
+      "text": "See documentation for data attribution.",
+      "link": "https://www.resonantenergy.app/documentation"
+    }
+]' --name 'Resonant Energy Geographies'
 } && {
   echo "Starting publishing process..."
   # If tileset creation is successful, publish tileset.
